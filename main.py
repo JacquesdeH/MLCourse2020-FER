499f9fa4b267b32218750d13a309e01b7a6fa802
JacquesdeH <jacquesdeh@buaa.edu.cn>
2020-12-09 00:47:15 +0800

    Done V23

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ EMBED_DIM = 128*7*7
 USE_SPARSE = True
 RHO = 0.02
 REGULIZER_WEIGHT = 1
-EPOCHS = 100
+EPOCHS = 150
 BATCH_SIZE = 128
 PRETRAIN_LR = 3e-5
-RESNET_BASE_LR = 1e-3
-RESNET_FT_LR = 5e-3
-RESNET_WEIGHT_DECAY = 5e-4
+RESNET_BASE_LR = 3e-3
+RESNET_FT_LR = 1e-2
+RESNET_WEIGHT_DECAY = 1e-5
 RESNET_DROPOUT = 0.5
 RESNET_MOMENTUM = 0.9
 MAX_NORM = 1.0
@@ -98,7 +98,7 @@ np.random.seed(RANDOM_SEED)
 if __name__ == '__main__':
     # timestamp = "20201205-030029"
     timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    model_name = "Resnet50-BS128-BigLR-MoreFC-Dropout-Noised-Epoch50-Version-22"
+    model_name = "Resnet50-BS128-WD-BigLR-MoreFC-Dropout-Noised-Epoch50-Version-23"
     model_name = timestamp + '-' + model_name
     use_model = 'resnet'  # 'svm' or 'resnet' or 'fc'
     instructor = Instructor(model_name, args)
