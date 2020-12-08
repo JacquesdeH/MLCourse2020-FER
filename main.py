afed9d61192f7416c26620b005e5f6045983186a
JacquesdeH <jacquesdeh@buaa.edu.cn>
2020-12-08 13:13:52 +0800

    Done V17

--- main.py
+++ main.py
@@ -16,12 +16,12 @@ EMBED_DIM = 128*7*7
 USE_SPARSE = True
 RHO = 0.02
 REGULIZER_WEIGHT = 1
-EPOCHS = 80
+EPOCHS = 50
 BATCH_SIZE = 32
 PRETRAIN_LR = 3e-5
-RESNET_BASE_LR = 1e-5
-RESNET_FT_LR = 1e-4
-RESNET_WEIGHT_DECAY = 5e-4
+RESNET_BASE_LR = 3e-5
+RESNET_FT_LR = 1e-3
+RESNET_WEIGHT_DECAY = 1e-5
 MAX_NORM = 1.0
 NUM_WORKERS = 1
 CUMUL_BATCH = 1
@@ -32,7 +32,7 @@ SVM_KER = 'rbf'
 SVM_MAX_ITER = -1
 WARMUP_RATE = 0.1
 UPSAMPLE_MODE = 'nearest'
-ADD_NOISE = 0.02
+ADD_NOISE = 0.01
 RESNET_DEPTH = 50
 
 # paths
@@ -92,7 +92,7 @@ np.random.seed(RANDOM_SEED)
 if __name__ == '__main__':
     # timestamp = "20201205-030029"
     timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-    model_name = "Resnet50-WD5e4-SmallLR-Noised-Epoch80-Version-16"
+    model_name = "Resnet50-WD1e5-Noised-Epoch50-Version-17"
     model_name = timestamp + '-' + model_name
     use_model = 'resnet'  # 'svm' or 'resnet' or 'fc'
     instructor = Instructor(model_name, args)
