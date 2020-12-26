27fd083e955b4b35bdccba9a9895c27a54a4726b
JacquesdeH <jacquesdeh@buaa.edu.cn>
2020-12-26 14:57:08 +0800

    Add new model to ensembler No.39

--- Ensembler.py
+++ Ensembler.py
@@ -41,6 +41,7 @@ submissions = {
     "20201210-164100-Cbam-Resnet50-Version-36--72471": 72.471,
     "20201210-164100-Cbam-Resnet50-Version-37--72862": 72.862,
     "20201210-164100-Cbam-Resnet50-Version-38--72053": 72.053,
+    "20201210-164100-Cbam-Resnet50-Version-39--72694": 72.694,
 }
 
 emotions = [emotion.name for emotion in LabelEnum]
